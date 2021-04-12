11776d47d1e755449ffb65b7d788c947b279442a
nokello2002 <nokello2002@gmail.com>
2021-04-12 17:02:02 +0300

    Assignment 2

---
+++ Nicholas_Assignment_2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 12 16:32:26 2021
+
+@author: HP
+"""
+
+first_name=input("Please enter first name: ")
+print (first_name)
+second_name=input("Please enter second name: ")
+print (second_name)
+full_names= (first_name + " " + second_name)
+country=input("Enter your country of origin: ")
+prof=input ("Enter nature of your current job: ")
+my_email=input("Enter your email address: ")
+my_phone=input("Enter your phone number: ")
+my_city=input("Enter your current city of residence: ")
+my_address=input("Enter your mailing address: ")
+print ("Name: " + full_names)
+print ("Country: " + country)
+print ("Profession: " + prof)
+print ("Email: " + my_email)
+print ("Phone No: " + my_phone)
+print ("City: " + my_city)
+print ("Address" + my_address)
+
+
