cea858e341e83ee4fe7857e7ae2afe728856aefd
nokello2002 <nokello2002@gmail.com>
2021-04-12 15:59:18 +0300

    Soon to be Data Scientist

---
+++ Nicholas_Assignment_1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 12 14:49:12 2021
+
+@author: HP
+"""
+
+#Enter first and last name
+names=input("Enter First and Last name")
+print (names)
+print()
+#Enter your goals for this class*
+print("What are your goals for this class -- long term goals?")
+goals=input("What would you like to be ...when all is said and done with the class?")
+print (goals)
